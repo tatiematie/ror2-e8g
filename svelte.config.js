d20e76c8ce44cbf0d4add4803a546bcdd62158e2
tatiematie <iamtatiematie@gmail.com>
2024-07-18 20:38:08 -0700

    config update

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,10 @@ import adapter from '@sveltejs/adapter-static';
 
 const config = {
   kit: {
-    // Ensure to change the paths.base to your repo name
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/ror2-e8g' : ''
+      base: process.env.NODE_ENV === 'production' ? '/your-repo-name' : ''
     },
     adapter: adapter({
-      // default options are shown. these are optional
       pages: 'build',
       assets: 'build',
       fallback: null
